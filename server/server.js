1e4c09352d72172d36da1449050433eb2c28a4b7
Jordan Walker <jwalker117@gmail.com>
2020-06-22 11:34:30 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+//Import routes for routing
+const robofriendRouter = require('./routes/robofriend.router');
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for angular requests
+app.use(express.static('build'));
+
+/** ---------- ROUTES ---------- **/
+app.use('/', robofriendRouter);
+
+/** ---------- START SERVER ---------- **/
+app.listen(port, function () {
+    console.log('Listening on port: ', port);
+});
