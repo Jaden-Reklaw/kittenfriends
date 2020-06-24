de8a640c355cd7c2d94b21c4172c04aac46500fb
Jordan Walker <jwalker117@gmail.com>
2020-06-24 14:19:14 -0500

    converted CardItem to class component for state and connecting delete and edit buttons

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 //Import Bootstrap components
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-const CardItem = ({name, email, id}) => {
-    return (
-        <Card 
-            style={{ width: '18rem', display:'inline-block', margin:'12px'}} 
-            className="text-center shadow p-3 mb-5 bg-white rounded"
-        >
-            <Card.Img variant="top" src={`https://robohash.org/${id}?set=set4`} />
-            <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>
-                {email}
-                </Card.Text>
-                <Button variant="primary" block="true">Edit</Button><br />
-                <Button variant="danger" block="true">Delete</Button>
-            </Card.Body>
-        </Card>
-    );
+class CardItem extends Component {
+    //Create state to change individual cards after they click edit
+
+    state = {
+        isEdit: false,
+        newData: {
+            name: this.props.name,
+            email: this.props.email
+        }
+    }
+
+    handleDelete = () => {
+        console.log('handleDelete pressed!');
+    }
+
+    handleEdit = () => {
+        console.log('handleEdit pressed!');
+    }
+
+    render() {
+        const {id, name, email} = this.props;
+        return (
+            <Card 
+                style={{ width: '18rem', display:'inline-block', margin:'12px'}} 
+                className="text-center shadow p-3 mb-5 bg-white rounded"
+            >
+                <Card.Img variant="top" src={`https://robohash.org/${id}?set=set4`} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                    {email}
+                    </Card.Text>
+
+                    {/* Edit Button */}
+                    <Button 
+                        variant="primary" 
+                        block="true"
+                        onClick={this.handleEdit}
+                    >Edit</Button>
+
+                    {/* Delete Button */}
+                    <Button 
+                        variant="danger" 
+                        block="true"
+                        onClick={this.handleDelete}
+                    >Delete</Button>
+
+                </Card.Body>
+            </Card>
+        );
+    }
 }
 
 export default CardItem;
